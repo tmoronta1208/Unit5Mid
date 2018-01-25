810a13ffc8b4bb4f32786d230b5c8fbb50622d6f
Tatiana Moronta <tatianamoronta@ac.c4q.nyc>
2018-01-24 20:31:14 -0500

    model class

---
+++ app/src/main/java/com/example/android/unit5mid/model/Results.java
@@ -0,0 +1,54 @@
+package com.example.android.unit5mid.model;
+
+/**
+ * Created by TatianaM on 1/24/18.
+ */
+
+public class Results {
+
+    private Name name;
+    private Location location;
+    private String email;
+    private String dob;
+    private String cell;
+
+    public Name getName() {
+        return name;
+    }
+
+    public void setName(Name name) {
+        this.name = name;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getCell() {
+        return cell;
+    }
+
+    public void setCell(String cell) {
+        this.cell = cell;
+    }
+}
